34ca9f597ca4e379eaccf5ef10d5f5c54d32f459
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-09 17:43:34 +0100

    Back up before coding log10 function as a subfunction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,26 +1,32 @@
 #include "binary_trees.h"
+#include <math.h>
 /**
- * binary_tree_height - function that goes through the tree and mesure height
- *@tree: tree to loop through
- *
- *Return: nothing
- */
-size_t binary_tree_height(const binary_tree_t *tree)
+  * binary_tree_inorder - in-order traversal of a tree
+  * @tree: constant ptr to binary_tree_t, root node of tree to traverse
+  * @func: ptr to function to call each node. Takes value of node as parameter
+  * Return: void
+  */
+size_t node_counter(const binary_tree_t *tree, size_t *node_count)
 {
-	int height = 0, height2 = 0;
-
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
 
-	if (tree->left != NULL)
-		height = binary_tree_height(tree->left);
-	if (tree->right != NULL)
-		height2 = binary_tree_height(tree->right);
+	node_counter(tree->left, node_count);
+	*node_count += 1;
+	node_counter(tree->right, node_count);
 
-	if (height > height2)
-		return (height + 1);
-	else
-		return (height2 + 1);
+	return (*node_count);
+}
+/**
+  * binary_tree_height - measures the height of a binary tree
+  * @tree: constant ptr to binary_tree_t, root node of tree to measure
+  * Return: size_t, unsigned int value of tree height
+  */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t node_count = 0;
+
+	node_count += node_counter(tree, &node_count);
+
+	return ((log10(node_count + 1) / log10(2)) - 1);
 }
