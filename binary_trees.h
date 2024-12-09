c10b1a83adfb190ab05d063327a8edc56f1da10e
batrivieredev <baptiste.rivierefr@gmail.com>
2024-12-09 10:28:33 +0100

    maj .h, leaf

--- binary_trees.h
+++ binary_trees.h
@@ -30,4 +30,7 @@ void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /* fonction left */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+/*delete*/
+int binary_tree_is_leaf(const binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
