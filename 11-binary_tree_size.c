ad34c6186ebdf1dbffb2beb9f259d7e08f83c061
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-10 10:25:04 +0100

    Julian's version of task 11 - counting while traversing in order

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,12 +1,19 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - count all child nodes including the root
- * @tree: tree to loop through
- * Return: size of the tree
+ * binary_tree_size - counts all nodes of a binary tree
+ * @tree: constant ptr to binary_tree_t, root node of tree to count nodes of
+ * Return: size_t, unsigned int nb of nodes
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t node_count = 0;
+
 	if (tree == NULL)
-		return (0);
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+		return (node_count);
+
+	node_count += binary_tree_size(tree->left);
+	node_count++;
+	node_count+=binary_tree_size(tree->right);
+
+	return (node_count);
 }
