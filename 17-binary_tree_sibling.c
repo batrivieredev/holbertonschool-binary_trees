93812787d19b6aed4b84b70b215862c3be3ab585
jmitchell35 <julian.m.mitchell@gmail.com>
2024-12-10 20:12:23 +0100

    Task 17, Julian's attempt

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,17 +1,24 @@
 #include "binary_trees.h"
 /**
- * *binary_tree_sibling - find sibling of tree node
- *@node: node to find the sibling of
- *
- *Return: address or null
+ * binary_tree_sibling - checks if BT node has a sibling
+ * @node: ptr to binary_tree_t, node to find sibling of
+ * Return: ptr to sibling
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent == NULL)
-		return (0);
-	if (node->parent->left == node)
-		return (node->parent->right);
-	return (node->parent->left);
+
+	if (node->parent->right == node && node->parent->left)
+	{
+		node = node->parent->left;
+		return (node);
+	}
+	else if (node->parent->left == node && node->parent->right)
+	{
+		node = node->parent->right;
+		return (node);
+	}
+	else
+		return (NULL);
 }
